43fd0fa4b4f04c1275b42adc498e43bda6130271
WolfMAN <aria.azadi.pour@protonmail.com>
2020-11-12 17:59:02 +0330

    connect mongoose and add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.dev'
+    }),
+    MongooseModule.forRoot(
+      process.env.DATABASE_URI
+        .replace('<password>', process.env.DATABASE_PASSWORD)
+        .replace('<dbname>', process.env.DATABASE_NAME)
+    )
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
